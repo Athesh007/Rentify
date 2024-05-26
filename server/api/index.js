3f4ad0d414c7591654785bfb09b3645bbfe76e96
Athesh007 <kavipprasath80@gmail.com>
2024-05-26 15:55:32 +0530

    fix

--- server/api/index.js
+++ server/api/index.js
@@ -11,8 +11,8 @@ connectDB();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/users", require("./routes/users"));
-app.use("/api/properties", require("./routes/properties"));
+app.use("/api/users", require("../routes/users"));
+app.use("/api/properties", require("../routes/properties"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
